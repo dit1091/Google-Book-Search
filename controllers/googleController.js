068fece20cf44a99f15164fe34d224ca4f66e808
Dannette Tejeda <dannettetejeda@gmail.com>
2021-01-31 17:34:42 -0500

    fixing config

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -3,7 +3,7 @@ const axios = require ("axios");
 module.exports = {
   getGoogleBooks: function(req, res) {
     const urlGoogle = `https://www.googleapis.com/books/v1/volumes?q=${req.params.keyword}` +
-      `&key=${process.env.GOOGLE_KEY}`;
+      `&key=${process.env.API_KEY}`;
     axios.get(urlGoogle)
     .then(response => res.json(response.data.items))
     .catch(error => res.status(400).json(error));
