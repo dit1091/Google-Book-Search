4f83ea087cbc19244239d8a2128df33ff3480835
Dannette Tejeda <dannettetejeda@gmail.com>
2021-01-31 17:54:58 -0500

    update

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const routes = require("./routes");
+
 const app = express()
 const PORT = process.env.PORT || 3000;
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Serve up static assets (usually on heroku)
+// Serve up static assets (heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
