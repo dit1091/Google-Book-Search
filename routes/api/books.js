f9c975c2eee77f5e45a33fe26399a89f81fbaa14
Dannette Tejeda <dannettetejeda@gmail.com>
2021-01-31 19:15:54 -0500

    update

--- routes/api/books.js
+++ routes/api/books.js
@@ -6,7 +6,10 @@ router.route("/")
   .get(booksController.findAll)
   .post(booksController.create);
 
-router.route("/:id")
+router
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
   .delete(booksController.remove);
 
 router.route("/search/:keyword")
